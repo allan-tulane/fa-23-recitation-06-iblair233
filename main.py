9e57915a826f2a520f7d2f64800e8eeae20da53b
iblair233 <126489949+iblair233@users.noreply.github.com>
2023-11-08 15:38:47 -0600

    Update first 3

--- main.py
+++ main.py
@@ -6,19 +6,31 @@ def fib_recursive(n, counts):
     """    
     counts[n] += 1
     ###TODO
-    pass
-    
+    counts[n] += 1
+    if n <= 1:
+        return n
+    else:
+        return fib_recursive(n - 1, counts) + fib_recursive(n - 2, counts)
 
     
 def fib_top_down(n, fibs):
     ###TODO
-    pass
+    if n <= 1:
+        return n
+    if fibs[n] != -1:
+        return fibs[n]
+    fibs[n] = fib_top_down(n - 1, fibs) + fib_top_down(n - 2, fibs)
+    return fibs[n]
 
 
 def fib_bottom_up(n):
     ###TODO
-    pass
-
+    fibs = [0] * (n + 1)
+    fibs[0] = 0
+    fibs[1] = 1
+    for i in range(2, n + 1):
+        fibs[i] = fibs[i - 1] + fibs[i - 2]
+    return fibs[n]
 
 
 
